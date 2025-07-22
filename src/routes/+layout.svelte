94e7aca03848d10a16a57b2520fe969d03dc60cc
Natthawut P. <45269469+v1b3x0r@users.noreply.github.com>
2025-07-22 09:26:43 +0000

    Pending changes exported from your codespace

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '../app.css';
+	import { Toaster } from '$lib/components/ui/sonner/';
+
+	let { children } = $props();
+</script>
+
+<div class="relative min-h-svh">
+	<Toaster richColors position="top-center" />
+
+	<!-- content container -->
+	<main class="container mx-auto">
+		{@render children()}
+	</main>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import { toast } from 'svelte-sonner';
+</script>
+
+<div class="mx-auto flex min-h-svh max-w-screen-sm flex-col">
+	<div class="flex flex-1 flex-col items-center justify-center gap-4 p-8 text-center">
+		<h1>SvelteKit 2.22.0 & ShadCN Svelte Boilerplate</h1>
+
+		<p>
+			Edit <code>src/routes/+layout.svelte</code> to see the layout changes.
+		</p>
+		<p>
+			Edit <code>src/routes/+page.svelte</code> to see the page changes.
+		</p>
+
+		<Button onclick={() => toast.success('Ready to MVP ✨')}>Show Toast</Button>
+	</div>
+</div>
